0d59ac04ab6b0a5166ac634e8606db47cd963e46
aryapradeep1 <aaryapradeep04@gmail.com>
2026-07-11 12:31:54 +0530

    change view

--- src/components/ViewStud.jsx
+++ src/components/ViewStud.jsx
@@ -1,72 +1,121 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import NaviBar from './NaviBar'
 
 const ViewStud = () => {
 
-        const[data,chanagedata]=useState([])
+    const [data, chanagedata] = useState([])
 
-        const fetchData=()=>{
-            axios.get("https://raw.githubusercontent.com/anishpdm/demo-json-api/refs/heads/main/placement.json").then(
+    const fetchData = () => {
+        axios.get("https://raw.githubusercontent.com/anishpdm/demo-json-api/refs/heads/main/placement.json").then(
 
-
-                (response)=>{
-                    chanagedata(response.data)
-                }
-
-            ).catch()
-        }
-
-         useEffect(
-            ()=>{
-                fetchData()
+            (response) => {
+                chanagedata(response.data)
             }
-         )
 
-  return (
-    <div>
+        ).catch((error) => {
+            console.log(error)
+        })
+    }
 
-        <div className="container">
-            <div className="row">
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+    useEffect(() => {
+        fetchData()
+    }, [])
 
-                    <div className="row g-3">
-                        {data.map(
-                            (value,inderx)=>{
-                                return(
-                                    <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+    return (
+        <div>
 
-                                <div class="card" >
-                <div class="card-body">
-                            <h5 class="card-title">{value.regNo}</h5>
-                        <p class="card-text">{value.fullName}</p>
-                         <p class="card-text">{value.aadharNumber}</p>
-                          <p class="card-text">{value.email}</p>
-                           <p class="card-text">{value.phoneNumber}</p>
-                            <p class="card-text">{value.branch}</p>
-                             <p class="card-text">{value.sslcMark}</p>
-                              <p class="card-text">{value.plusTwoMark}</p>
-                               <p class="card-text">{value.pgDegree}</p>
-                                <p class="card-text">{value.ugDegree}</p>
-                                 <p class="card-text">{value.pgMark}</p>
-                                  <p class="card-text">{value.ugMark}</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                            </div>
+            <NaviBar />
 
-                        </div>
+            <div className="container mt-4">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <div className="row g-4">
+
+                            {data.map((value, inderx) => {
+
+                                return (
+
+                                    <div
+                                        key={inderx}
+                                        className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4"
+                                    >
+
+                                        <div className="card h-100 shadow">
+
+                                            <div className="card-body d-flex flex-column">
+
+                                                <h5 className="card-title text-primary">
+                                                    {value.regNo}
+                                                </h5>
+
+                                                <p className="card-text">
+                                                    <b>Name :</b> {value.fullName}
+                                                </p>
+
+                                                <p className="card-text">
+                                                    <b>Aadhar :</b> {value.aadharNumber}
+                                                </p>
+
+                                                <p className="card-text">
+                                                    <b>Email :</b> {value.email}
+                                                </p>
+
+                                                <p className="card-text">
+                                                    <b>Phone :</b> {value.phoneNumber}
+                                                </p>
+
+                                                <p className="card-text">
+                                                    <b>Branch :</b> {value.branch}
+                                                </p>
+
+                                                <p className="card-text">
+                                                    <b>SSLC :</b> {value.sslcMark}
+                                                </p>
+
+                                                <p className="card-text">
+                                                    <b>Plus Two :</b> {value.plusTwoMark}
+                                                </p>
+
+                                                <p className="card-text">
+                                                    <b>UG Degree :</b> {value.ugDegree}
+                                                </p>
+
+                                                <p className="card-text">
+                                                    <b>UG Mark :</b> {value.ugMark}
+                                                </p>
+
+                                                <p className="card-text">
+                                                    <b>PG Degree :</b> {value.pgDegree}
+                                                </p>
+
+                                                <p className="card-text">
+                                                    <b>PG Mark :</b> {value.pgMark}
+                                                </p>
+
+                                                <a href="#" className="btn btn-primary mt-auto">
+                                                    View Details
+                                                </a>
+
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+
                                 )
 
-                            }
-                        )}
+                            })}
+
+                        </div>
+
                     </div>
-
-
                 </div>
             </div>
-        </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default ViewStud
